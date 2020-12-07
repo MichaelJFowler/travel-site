642e70b7b7c6a922207fd93f886fa0eef8012898
Michael <mike@fowler.co.za>
2020-12-07 16:05:01 +0200

    Second commit for Webpack

--- app/bundled.js
+++ app/bundled.js
@@ -10,6 +10,6 @@
 /*!***********************************!*
   !*** ./app/assets/scripts/App.js ***!
   \***********************************/
-eval("alert(\"Hello this is just a test\")\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
+eval("alert(\"Hello this is just another test???\")\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
 /******/ })()
 ;
