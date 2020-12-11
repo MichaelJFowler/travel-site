5dbf69a220621d69424ac297cd9d546bb5cdb4c6
Michael <mike@fowler.co.za>
2020-12-11 11:44:48 +0200

    Finished Sticky Header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
 let mobileMenu = new MobileMenu();
+// create two instances of the class for the featured items and testimonials
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-
+let stickyHeader = new StickyHeader();
 
 // allow hot module replacement from webpack-dev-server
 if (module.hot) {
