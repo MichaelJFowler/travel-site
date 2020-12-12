dd46360324f64bde4e4d07fcba443f0cf69cdae4
Michael <mike@fowler.co.za>
2020-12-12 10:44:56 +0200

    Finished Modal Section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,14 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 let mobileMenu = new MobileMenu();
 // create two instances of the class for the featured items and testimonials
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let stickyHeader = new StickyHeader();
+new Modal();
 
 // allow hot module replacement from webpack-dev-server
 if (module.hot) {
