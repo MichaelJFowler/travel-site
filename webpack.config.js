76a9d7001744f80228a9f2419688e2f94ab3fabe
Michael <mike@fowler.co.za>
2020-12-14 09:57:47 +0200

    Changed dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const postCSSPlugins = [
 class RunAfterCompile {
     apply(compiler) {
         compiler.hooks.done.tap('Copy images', function () {
-            fse.copySync('./app/assets/images', './dist/assets/images')
+            fse.copySync('./app/assets/images', './docs/assets/images')
 
         });
     }
@@ -114,7 +114,7 @@ if (currentTask == 'build') {
     config.output = {
         filename: '[name].[chunkhash].js',
         chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     }
     config.mode = 'production'
     // help visitors download less data in the long run
